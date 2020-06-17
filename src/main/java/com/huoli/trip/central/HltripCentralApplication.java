391e091cd0469809bd4f626dd8dbd0e12c896127
顾刘川 <glcworld@163.com>
2020-06-17 15:07:16 +0800

    Initial commit

---
+++ src/main/java/com/huoli/trip/central/HltripCentralApplication.java
@@ -0,0 +1,13 @@
+package com.huoli.trip.central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HltripCentralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HltripCentralApplication.class, args);
+    }
+
+}
