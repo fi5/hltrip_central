374e324c7e7cee092a4964ec4515cc18c1f98c51
wangdeming <1179441447@qq.com>
2020-06-30 18:49:41 +0800

    1.预定前校验接口文档定义

--- hltrip_central_web/src/main/java/com/huoli/trip/central/web/service/OrderConsumerService.java
+++ hltrip_central_web/src/main/java/com/huoli/trip/central/web/service/OrderConsumerService.java
@@ -3,9 +3,9 @@ package com.huoli.trip.central.web.service;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.huoli.trip.supplier.api.YcfOrderService;
 import com.huoli.trip.supplier.self.yaochufa.vo.YcfBookCheckReq;
+import com.huoli.trip.supplier.self.yaochufa.vo.YcfCreateOrderReq;
+import com.huoli.trip.supplier.self.yaochufa.vo.YcfPayOrderReq;
 import org.springframework.stereotype.Component;
-
-import java.util.Date;
 
 /**
  * 接收供应商层的dubbo服务并做业务处理
@@ -14,6 +14,8 @@ import java.util.Date;
 public class OrderConsumerService {
     @Reference( group = "hllx")
     YcfOrderService ycfOrderService;
+
+//*************************************【要出发】*******************************************
 
     public Object getYaochufaOrderStatus(String orderId){
         ycfOrderService.getOrder(orderId);
@@ -25,6 +27,15 @@ public class OrderConsumerService {
         return ycfOrderService.getCheckInfos(checkRequest);
     }
 
+    public Object createOrder(YcfCreateOrderReq createOrderReq){
+        return ycfOrderService.createOrder(createOrderReq);
+    }
+
+    public Object payOrder(YcfPayOrderReq payOrderReq){
+        return ycfOrderService.payOrder(payOrderReq);
+    }
+
+//*************************************【笛风云】*******************************************
     public Object getDiFengYunOrderStatus(String orderId){
         return null;
     }
