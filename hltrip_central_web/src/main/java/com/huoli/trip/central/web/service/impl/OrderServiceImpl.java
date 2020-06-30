69237b1e090c6ed4c94f118418c801332767d44c
顾刘川 <glcworld@163.com>
2020-06-30 22:25:35 +0800

    产品

---
+++ hltrip_central_web/src/main/java/com/huoli/trip/central/web/service/impl/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+package com.huoli.trip.central.web.service.impl;
+
+import com.huoli.trip.central.api.OrderService;
+import com.huoli.trip.common.vo.request.BookCheckReq;
+import com.huoli.trip.common.vo.request.OrderStatusRequest;
+
+/**
+ * 描述：订单方法处理<br>
+ * 版权：Copyright (c) 2011-2020<br>
+ * 公司：活力天汇<br>
+ * 作者：顾刘川<br>
+ * 版本：1.0<br>
+ * 创建日期：2020/6/30<br>
+ */
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public Object getOrderStatus(OrderStatusRequest request) {
+        return null;
+    }
+
+    @Override
+    public Object getCheckInfos(BookCheckReq req) {
+        return null;
+    }
+}
