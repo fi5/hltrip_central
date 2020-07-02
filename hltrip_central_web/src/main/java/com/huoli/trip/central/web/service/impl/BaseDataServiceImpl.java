e795cdc8173afc3ae8f99e0544e7022084437f95
zhouwenbin <zhouwb@133.cn>
2020-07-02 15:57:50 +0800

    city实现

--- hltrip_central_web/src/main/java/com/huoli/trip/central/web/service/impl/BaseDataServiceImpl.java
+++ hltrip_central_web/src/main/java/com/huoli/trip/central/web/service/impl/BaseDataServiceImpl.java
@@ -2,9 +2,11 @@ package com.huoli.trip.central.web.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.huoli.trip.central.api.IBaseDataService;
+import com.huoli.trip.central.web.dao.CityDao;
 import com.huoli.trip.common.entity.CityPO;
 import com.huoli.trip.common.vo.response.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -16,8 +18,13 @@ import java.util.List;
 @Slf4j
 @Service(timeout = 10000,group = "hltrip")
 public class BaseDataServiceImpl implements IBaseDataService{
+
+	@Autowired
+	CityDao cityDao;
+
 	@Override
 	public BaseResponse<List<CityPO>> queryCitys() {
-		return null;
+		List<CityPO> citys = cityDao.queryCitys();
+		return BaseResponse.success(citys);
 	}
 }
