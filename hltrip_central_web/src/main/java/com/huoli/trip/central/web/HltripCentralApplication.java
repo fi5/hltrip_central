8bbb9139768ae906f8764af9e7c5d2de043af1cd
fengzhiqiang <fengzq@133.cn>
2020-07-10 17:42:45 +0800

    1.异常优化

--- hltrip_central_web/src/main/java/com/huoli/trip/central/web/HltripCentralApplication.java
+++ hltrip_central_web/src/main/java/com/huoli/trip/central/web/HltripCentralApplication.java
@@ -3,14 +3,19 @@ package com.huoli.trip.central.web;
 import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(exclude = {RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class,
-        DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+        DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class,
+        MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@ImportResource({"classpath:disconf-config.xml"})
 @DubboComponentScan(value = "com.huoli.trip.central.web")
 @ComponentScan({"com.huoli.trip"})
 public class HltripCentralApplication {
