c170c0b90a2de517859bbf8e808d58d8eff3fdc0
wangdm <1179441447@qq.com>
2020-07-14 13:40:58 +0800

    1.创建订单加点异常逻辑处理

--- hltrip_central_web/src/main/java/com/huoli/trip/central/web/converter/SupplierErrorMsgTransfer.java
+++ hltrip_central_web/src/main/java/com/huoli/trip/central/web/converter/SupplierErrorMsgTransfer.java
@@ -2,6 +2,7 @@ package com.huoli.trip.central.web.converter;
 
 import com.huoli.trip.common.constant.CentralError;
 import com.huoli.trip.common.vo.response.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 描述: <br> 供应商异常转义
@@ -11,12 +12,12 @@ import com.huoli.trip.common.vo.response.BaseResponse;
  * 版本：1.0<br>
  * 创建日期：2020/7/14<br>
  */
+@Slf4j
 public class SupplierErrorMsgTransfer {
     public static BaseResponse buildMsg(String msg){
         switch (msg){
             case "请填写联系人手机号码" :
             case "请填写联系人姓名" :
-            case "无价格信息" :
             case "门票使用日期不在入住范围内" :
             case "餐券使用日期不在入住范围内" :
             case "餐券使用日期不在票范围内" :
@@ -28,7 +29,7 @@ public class SupplierErrorMsgTransfer {
             case "创建订单失败，购买数超出最多购买数" :
             case "创建订单失败，入住晚数少于最小入住晚数" :
             case "创建订单失败，入住晚数超出最大入住晚数" :
-                return BaseResponse.fail(401,msg,null);
+                return BaseResponse.fail(400,msg,null);
             case "该产品不存在" :
                 return BaseResponse.fail(CentralError.ERROR_NO_PRODUCT_SUPPLIER);
             case "该产品已下架" :
@@ -36,14 +37,16 @@ public class SupplierErrorMsgTransfer {
             case "订单已存在" :
                 return BaseResponse.fail(CentralError.ERROR_ORDER_ISEXIST_SUPPLIER);
             case "价格不存在" :
+            case "无价格信息" :
             case "产品价格不得低于最低售价" :
             case "价格不存在或者售罄" :
             case "订单总价错误" :
                 return BaseResponse.fail(2014,msg,null);
             case "库存不足" :
                 return BaseResponse.fail(CentralError.ERROR_ORDER_STOCK_SUPPLIER);
-            default:break;
+            default:
+                log.error("错误异常描述是 ：{}",msg);
+                return BaseResponse.fail(400,msg,null);
         }
-       return BaseResponse.fail(CentralError.ERROR_BAD_REQUEST);
     }
 }
