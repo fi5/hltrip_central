d5011cb5affca497c6927ca1e568263ef8a5b10d
fengzhiqiang <fengzq@133.cn>
2020-10-19 15:07:21 +0800

    1.提前预订

--- hltrip_central_web/src/main/java/com/huoli/trip/central/web/service/impl/ProductServiceImpl.java
+++ hltrip_central_web/src/main/java/com/huoli/trip/central/web/service/impl/ProductServiceImpl.java
@@ -165,6 +165,9 @@ public class ProductServiceImpl implements ProductService {
             ProductPriceCalendarResult result = new ProductPriceCalendarResult();
 
             final PricePO pricePo = productDao.getPricePos(productPriceReq.getProductCode());
+            ProductPO productPO = productDao.getTripProductByCode(productPriceReq.getProductCode());
+            // 提前预订天数
+            Integer aheadDays = productPO.getBookAheadMin() == null ? null : (productPO.getBookAheadMin() / 60 / 24);
             if(null==pricePo || CollectionUtils.isEmpty(pricePo.getPriceInfos()))
                 return BaseResponse.fail(CentralError.NO_RESULT_ERROR);
             List<PriceInfo> priceInfos = Lists.newArrayList();
@@ -186,6 +189,10 @@ public class ProductServiceImpl implements ProductService {
                 BeanUtils.copyProperties(entry, target);
                 if(target.getSalePrice()!=null && target.getSalePrice().floatValue()<=0)//销售价格为0的去掉
                     continue;
+                // 预订的日期 - 今天 >= 提前预定天数  的不返回；
+                if(aheadDays != null && DateTimeUtil.getDateDiffDays(entry.getSaleDate(), new Date()) >= aheadDays ){
+                    continue;
+                }
                 priceInfos.add(target);
 //                log.info("这里的日期:" + CommonUtils.dateFormat.format(target.getSaleDate()));
             }
