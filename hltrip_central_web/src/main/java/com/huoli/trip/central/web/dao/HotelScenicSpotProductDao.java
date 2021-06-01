bb75ad9e607a0299ac51c92dea31a72009766668
fengzhiqiang <fengzq@133.cn>
2021-06-01 19:01:55 +0800

    1.新版价格详情、价格计算

---
+++ hltrip_central_web/src/main/java/com/huoli/trip/central/web/dao/HotelScenicSpotProductDao.java
@@ -0,0 +1,21 @@
+package com.huoli.trip.central.web.dao;
+
+import com.huoli.trip.common.entity.mpo.hotelScenicSpot.HotelScenicSpotProductMPO;
+
+/**
+ * 描述：<br/>
+ * 版权：Copyright (c) 2011-2020<br>
+ * 公司：活力天汇<br>
+ * 作者：冯志强<br>
+ * 版本：1.0<br>
+ * 创建日期：2021/6/1<br>
+ */
+public interface HotelScenicSpotProductDao {
+
+    /**
+     * 根据id获取产品
+     * @param productId
+     * @return
+     */
+    HotelScenicSpotProductMPO getProductById(String productId);
+}
