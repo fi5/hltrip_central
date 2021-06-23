801aa2199b5dde20e1f78eb48d584e8d69009338
fengzhiqiang <fengzq@133.cn>
2021-06-23 14:26:30 +0800

    1.推荐亲子活动

--- hltrip_central_web/src/main/java/com/huoli/trip/central/web/service/impl/ProductServiceImpl.java
+++ hltrip_central_web/src/main/java/com/huoli/trip/central/web/service/impl/ProductServiceImpl.java
@@ -1264,6 +1264,7 @@ public class ProductServiceImpl implements ProductService {
         recommendProduct.setCategory(rb.getCategory());
         recommendProduct.setBookDay(rb.getBookDay());
         recommendProduct.setRecommendDesc(rb.getRecommendDesc());
+        recommendProduct.setSubTitle(rb.getSubTitle());
         recommendProduct.setTags(rb.getTags());
         return recommendProduct;
     }
